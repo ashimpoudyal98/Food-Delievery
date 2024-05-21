ba6674e78aa1a74dc1efc502a51d48e3596da9f8
ashimpoudyal98 <75020083+ashimpoudyal@users.noreply.github.com>
2024-05-21 22:25:44 +0545

    First Commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -11,11 +11,33 @@ const Footer = () => {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora
             molestiae, veritatis fugiat reiciendis quibusdam quo et eligendi
           </p>
-          <div className="footer-social-icons"></div>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
         </div>
-        <div className="footer-content-center"></div>
-        <div className="footer-content-right"></div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delievery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+12345678910</li>
+            <li>a@tomato.com</li>
+          </ul>
+        </div>
       </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 @ Tomato.com - All Right Reserved.
+      </p>
     </div>
   );
 };
